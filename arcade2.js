03ad9da96eb5fd4d3244c94c8976fe814c4190b6
nicktamburro <nicholastamburro@gmail.com>
2018-03-09 22:34:05 -0800

    commented

--- arcade2.js
+++ arcade2.js
@@ -1,3 +1,9 @@
+/*//Given a year, return the century it is in. The first 
+century spans from the year 1 up to and including the 
+year 100, the second - from the year 101 up to and 
+including the year 200, etc.*/
+
+
 function centuryFromYear(year) {
     if (year > 0 && year < 101){
         return 1;
