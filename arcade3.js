7b34dbf2889dff763c8d0b40680cb5db05d1c169
nicktamburro <nicholastamburro@gmail.com>
2018-03-10 21:25:36 -0800

    checkPalindrome

---
+++ arcade3.js
@@ -0,0 +1,8 @@
+//Given the string, check if it is a palindrome.
+
+function checkPalindrome(inputString){
+    
+    let backwardsString = inputString.split("").reverse().join("");
+
+    return (inputString === backwardsString);
+}
