936e0ed8cc3fe7ef4ce9df87626cc352171febca
nicktamburro <nicholastamburro@gmail.com>
2018-03-12 11:34:11 -0700

    added Mocha test

--- arcade1.js
+++ arcade1.js
@@ -1,8 +1,19 @@
 //write a function that returns the sum of two numbers: 
-npm install --global mocha
+var assert = require('assert');
+
+
+//Mocha test
+describe('add', function() {
+    it('returns the sum of 2 parameters', function() {
+
+        var result1 = add(1, 10);
+
+      assert.equal(result1, 11, 'add(1, 10) is 11');
+    });
+});
 
 function add(param1, param2){
-    console.log(param1 + param2);
+    return(param1 + param2);
 }
 
 add(3, 4);
