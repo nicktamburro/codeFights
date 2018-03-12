534e2f4d8364f9a2288b00a26a6ff2ebbf8d9374
nicktamburro <nicholastamburro@gmail.com>
2018-03-12 11:25:46 -0700

    switched to node for testing

--- arcade1.js
+++ arcade1.js
@@ -1,5 +1,9 @@
 //write a function that returns the sum of two numbers: 
+npm install --global mocha
 
 function add(param1, param2){
-    return (param1 + param2);
+    console.log(param1 + param2);
 }
+
+add(3, 4);
+add(10, 3487);
