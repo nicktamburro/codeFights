d05d2dbd51e9ce4054c5e7c37a391711352a88b7
nicktamburro <nicholastamburro@gmail.com>
2018-03-14 22:01:39 -0700

    wrote mocha test for makeArrayConsecutive

---
+++ test/6-makeArrayConsecutive-test.js
@@ -0,0 +1,20 @@
+//the tests I want to do:
+// it adds up the missing spaces for numbers
+
+//so like, [1, 2, 4, 5, 7, 8, 10] would be 3... I think I'm skipping some steps...
+
+var assert = require('assert');
+var makeArrayConsecutive = require('../6-makeArrayConsecutive.js');
+
+describe('makeArrayConsecutive', function() {
+    it('gives total of missing numbers in a consecutive array', function() {
+
+        let result1 = makeArrayConsecutive([1, 2, 4, 5, 7, 8, 10]);
+        let result2 = makeArrayConsecutive([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+      assert.equal(result1, 3, 'makeArrayConsecutive([1, 2, 4, 5, 7, 8, 10]) is 3');
+      assert.equal(result2, 0, 'makeArrayConsecutive([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) is 0');
+    });
+});
+
+
