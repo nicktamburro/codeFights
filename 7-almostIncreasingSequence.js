37ffc5a132040b193f0cd33634f0bb399dedbd2f
nicktamburro <nicholastamburro@gmail.com>
2018-03-25 20:58:30 -0700

    getting closer on increasing sequence

--- 7-almostIncreasingSequence.js
+++ 7-almostIncreasingSequence.js
@@ -2,10 +2,17 @@ function almostIncreasingSequence(sequence) {
 let wrong = 0;
     
     for (i = 0 ; i<sequence.length; i++){
-        if (sequence[i] != sequence[i+1] - 1){
-    wrong++;
-        }if (sequence[i-1] === sequence[i+1] - 1)
-            wrong--;
+        if (sequence[i] >= sequence[i+1]){
+            wrong++;
+        }
+    
+        
+    }     
+       console.log(wrong);
+    if (wrong > 1){
+        return false;
+    }else{
+        return true;
     }
-    console.log(wrong);
+ 
 }
