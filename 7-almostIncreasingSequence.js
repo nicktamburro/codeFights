447e60ce15c946afe1160d9fd848f59e26fcfac4
nicktamburro <nicholastamburro@gmail.com>
2018-03-27 22:16:11 -0700

    getting closer on increasing sequence

--- 7-almostIncreasingSequence.js
+++ 7-almostIncreasingSequence.js
@@ -1,9 +1,16 @@
+//better... now only failing [1, 2, 3, 4, 3, 6] (getting 2 = false, should return true)
+//and [3, 5, 67, 98, 3] (getting 2 = false, should return true)
+
 function almostIncreasingSequence(sequence) {
 let wrong = 0;
     
     for (i = 0 ; i<sequence.length; i++){
         if (sequence[i] >= sequence[i+1]){
             wrong++;
+        }else{
+            if (sequence[i]>=sequence[i+2]){
+                wrong++;
+            }
         }
     
         
