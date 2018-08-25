1e92d74cc5cafb86d292f343460706b84fc49f4d
nicktamburro <nicholastamburro@gmail.com>
2018-08-24 22:20:37 -0700

    check palindrome, but in java

---
+++ 3-checkPalindrome.java
@@ -0,0 +1,19 @@
+boolean checkPalindrome(String inputString) {
+	//remember in Java we have to declare what the function returns... boolean
+	//AND have to declare what the arguments/parameters will be..... String
+
+//loop through the characters in the string
+   for (int i= 0; i<inputString.length(); i++){
+   	//I don't totally get the parens after length, because length is a function?
+      if(inputString.charAt(i) != inputString.charAt(inputString.length() -i - 1))
+      	//but yeah, so it checks if the char at 0 matches the char at the end
+      	//then if char at 1 matches 2nd to last...
+
+      	//so remember charAt!
+      	//and .length()
+         return false;
+      
+    
+   }
+        return true;
+}
