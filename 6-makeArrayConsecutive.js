89ef2a300e61eba412470e2f2c9260a5990c5a73
nicktamburro <nicholastamburro@gmail.com>
2018-08-26 14:08:21 -0700

    makeArrayConsecutive.js

---
+++ 6-makeArrayConsecutive.js
@@ -0,0 +1,21 @@
+function makeArrayConsecutive2(statues) {
+
+    let count = 0;
+    let sorted = statues.sort(function(a, b){return a - b});
+    console.log(sorted);
+    
+    if(sorted.length <= 1){
+        return 0;
+    }
+    else{
+    for(i = 0; i<sorted.length - 1; i++){
+        count += (sorted[i+1] - sorted[i]) - 1;
+      console.log(count);
+    }
+    }
+    if(count > 0){
+    return count;
+    }else{
+        return 0;
+    }
+}
