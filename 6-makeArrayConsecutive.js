e793fa2c93e6aa1108dd5af4ba07004142e9f742
nicktamburro <nicholastamburro@gmail.com>
2018-08-26 14:11:05 -0700

    fixed arrayConsecutive

--- 6-makeArrayConsecutive.js
+++ 6-makeArrayConsecutive.js
@@ -2,7 +2,7 @@ function makeArrayConsecutive2(statues) {
 
     let count = 0;
     let sorted = statues.sort(function(a, b){return a - b});
-    console.log(sorted);
+    //console.log(sorted);
     
     if(sorted.length <= 1){
         return 0;
@@ -10,12 +10,10 @@ function makeArrayConsecutive2(statues) {
     else{
     for(i = 0; i<sorted.length - 1; i++){
         count += (sorted[i+1] - sorted[i]) - 1;
-      console.log(count);
+      //console.log(count);
     }
-    }
-    if(count > 0){
+    } 
+   
     return count;
-    }else{
-        return 0;
-    }
+  
 }
