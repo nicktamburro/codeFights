5e1668a9603e17d13aff540d7f36e1ab6724f150
nicktamburro <nicholastamburro@gmail.com>
2018-08-26 21:58:58 -0700

    #6 in java

---
+++ 6-makeArrayConsecutive.java
@@ -0,0 +1,10 @@
+int makeArrayConsecutive2(int[] statues) {
+
+    Arrays.sort(statues);
+    int count = 0;
+   
+    for(int i = 0; i<statues.length - 1; i++){
+        count+= (statues[i+1] - statues[i]) - 1;
+    }
+    return count;
+}
--- 6-makeArrayConsecutive.js
+++ 6-makeArrayConsecutive.js
@@ -1,9 +1,13 @@
 function makeArrayConsecutive2(statues) {
 
     let count = 0;
+
+    //THIS is a thing I just need to memorize... 
+    //_____.sort(function(a,b){return a - b})
+    //which tells it HOW we want it to sort for us... 
     let sorted = statues.sort(function(a, b){return a - b});
     //console.log(sorted);
-    
+    //.sort(function(a,b){return a - b});
     if(sorted.length <= 1){
         return 0;
     }
