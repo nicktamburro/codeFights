fb604fa501e4379c82a3a1812c6c1450d46b31c5
nicktamburro <nicholastamburro@gmail.com>
2018-10-02 14:32:54 -0700

    java century from year calculator

--- 2-centuryFromYear.java
+++ 2-centuryFromYear.java
@@ -8,3 +8,11 @@ int centuryFromYear(int year) {
     
 
 }
+
+int centuryFromYear2(int year){
+    if(year % 100 == 0){
+        return year / 100;
+    }else{
+        return year / 100 + 1;
+    }
+}
